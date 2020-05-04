d93a68d658724c553589f4cf5a7cde8bc485c1f3
Alex Duveneck <alexander.duveneck@gmail.com>
2020-05-04 10:34:10 -0700

    Update to include yarn installation, and changes to Python to make sure we install pip

--- program_installation_setup.sh
+++ program_installation_setup.sh
@@ -9,16 +9,19 @@
 ################################################################################
 
 
-# curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-# node --version
-# sudo apt-get install software-properties-common
-# sudo apt-get install gcc g++ make && sudo apt-get install yarn
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node --version
+sudo apt-get install software-properties-common
+sudo apt-get install gcc g++ make && sudo apt-get install yarn
 
-# Docker
-# Install in Windows Ecosystem via: https://hub.docker.com/editions/community/docker-ce-desktop-windows
+################################################################################
+#                                     Yarn                                     #
+################################################################################
 
-
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
 
 ################################################################################
 #                                  PostgreSQL                                  #
@@ -28,26 +31,32 @@
 # https://medium.com/@harshityadav95/postgresql-in-windows-subsystem-for-linux-wsl-6dc751ac1ff3
 sudo apt-get install postgresql
 
-# starting service
-# sudo service postgresql start
+starting service
+sudo service postgresql start
 
-# Connecting to postgres
-# sudo -u postgres ps
+Connecting to postgres
+sudo -u postgres ps
 
 ################################################################################
 #                                    Python                                    #
 ################################################################################
+
 # https://linuxize.com/post/how-to-install-python-3-7-on-ubuntu-18-04/ 
+# sudo ln -fs /usr/bin/python3.7 /usr/bin/python   # had to do stuff with yarn FML
 # first 3 steps roughly same. Last from Javin
-sudo apt update
-sudo apt install software-properties-common
+sudo apt-get update
+sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install libpq-dev libssl-dev python3.7-dev
+sudo apt-get install -y python3.7 python-dev  libpq-dev libssl-dev apt-transport-https ca-certificates curl gnupg-agent python3-pip python3.7-dev
+sudo pip3 install pipenv
+sudo apt update
 
 ################################################################################
 #                                    Docker                                    #
 ################################################################################
 # https://docs.docker.com/compose/install/ # followed for Linux
+# Docker
+# Install in Windows Ecosystem via: https://hub.docker.com/editions/community/docker-ce-desktop-windows
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
