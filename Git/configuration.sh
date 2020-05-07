f759fd43c5c8ea0fd86ecc6284f900eb56dce603
Asduveneck <alexander.duveneck@gmail.com>
2020-05-07 16:23:13 -0700

    Update notes and extension list for future reinstallations

--- Git/configuration.sh
+++ Git/configuration.sh
@@ -5,6 +5,7 @@ git config --list
 
 # Generate SSH keys
 cd ~/.ssh
+# cd ~ && mkdir ./.ssh # if the above fails
 ls # check that everything is empty
 ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
 # Take your id_rsa.pub and add it as an SSH key for your github
