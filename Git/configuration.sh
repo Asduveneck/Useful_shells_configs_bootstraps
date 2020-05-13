ad2e5b0029031f0d7b1c621dd50f666661c3455b
Alexander Duveneck <alexander.duveneck@gmail.com>
2020-05-12 21:42:04 -0700

    add steps to customize ssh key filenames

--- Git/configuration.sh
+++ Git/configuration.sh
@@ -9,5 +9,12 @@ cd ~/.ssh
 ls # check that everything is empty
 ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
 # Take your id_rsa.pub and add it as an SSH key for your github
+# you can also customize the ssh key name and configure it for github in a config file:
+# https://superuser.com/questions/232373/how-to-tell-git-which-private-key-to-use
+
+# host github.com
+#  HostName github.com
+#  IdentityFile ~/.ssh/id_rsa_github
+#  User git
 
 # https://help.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
