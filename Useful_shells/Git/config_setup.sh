904457e26cac42850fbecebbed5d2963c8c3865e
Asduveneck <alexander.duveneck@gmail.com>
2021-03-21 13:33:24 -0700

    Update instructions and link to docs

--- Useful_shells/Git/config_setup.sh
+++ Useful_shells/Git/config_setup.sh
@@ -1,20 +1,25 @@
+# Setting up your username. Uncomment below and fill in accordingly
+
 # sudo apt-get install git
 # git config --global user.name "USERNAME"
 # git config --global user.email "EMAIL"
 git config --list
 
+# Setting up SSH Key
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+
 # Generate SSH keys
 cd ~/.ssh
-# cd ~ && mkdir ./.ssh # if the above fails
-ls # check that everything is empty
-ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+# mkdir ~/.ssh && cd ~/.ssh # if the above fails
+ssh-keygen -t ed25519 -C "alexander.duveneck@gmail.com"
 # Take your id_rsa.pub and add it as an SSH key for your github
+
 # you can also customize the ssh key name and configure it for github in a config file:
 # https://superuser.com/questions/232373/how-to-tell-git-which-private-key-to-use
 
-# host github.com
-#  HostName github.com
-#  IdentityFile ~/.ssh/id_rsa_github
-#  User git
+# Host github.com
+#   HostName github.com
+#   IdentityFile ~/.ssh/id_rsa_github
 
+# Be sure to chmood the key and config appropriately (400 & 600)
 # https://help.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
